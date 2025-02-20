b0dc65bab464494dc37ed1df6e0906b3927e3215
mujtabadamu <mujtabadamu@gmail.com>
2025-02-20 10:38:36 +0100

    add skeleton loader and receipt adjustment

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import "react-loading-skeleton/dist/skeleton.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
